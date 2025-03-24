e78edc181874544f724c8b4d4729a09f5aa21768
Ростислав Грачев <gracevrostislav5@gmail.com>
2025-03-24 10:40:10 +0700

    Немного изменил валидацию полей в модели

--- Api-shop/Models/User.cs
+++ Api-shop/Models/User.cs
@@ -6,11 +6,13 @@ namespace WebApiUseSqlLiteItog.Models
     public class User
     {
         [Key]
+        [EmailAddress]
         public string? Email { get; set; }
 
         public string? Fio { get; set; }
 
         [Required]
+        [MinLength(6)]
         public string? Password { get; set; }
 
         public string UserToken { get; set; } = "";
